8495da543e23d1c949a7651b5450d1248f72a7ad
z <zhaohuaxs.sdu@gmail.com>
2022-12-03 20:58:58 +0800

    initial

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
+import ElementUI from 'element-ui'
+// import 'element-ui/lib/theme-chalk/index.css'
 
-import './assets/main.css'
+// 加载全局样式
+import './styles/index.scss'
 
-const app = createApp(App)
+Vue.use(ElementUI)
 
-app.use(router)
+Vue.config.productionTip = false
 
-app.mount('#app')
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,71 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Vue from 'vue'
+import VueRouter, { RouteConfig } from 'vue-router'
+import Layout from '@/layout/index.vue'
 
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+Vue.use(VueRouter)
+
+// 路由配置规则
+const routes: Array<RouteConfig> = [
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import(/* webpackChunkName: 'login' */ '@/views/login/index.vue')
+  },
+  {
+    path: '/',
+    component: Layout,
+    children: [
+      {
+        path: '', // 默认子路由
+        name: 'home',
+        component: () => import(/* webpackChunkName: 'home' */ '@/views/home/index.vue')
+      },
+      {
+        path: '/role',
+        name: 'role',
+        component: () => import(/* webpackChunkName: 'role' */ '@/views/role/index.vue')
+      },
+      {
+        path: '/menu',
+        name: 'menu',
+        component: () => import(/* webpackChunkName: 'menu' */ '@/views/menu/index.vue')
+      },
+      {
+        path: '/resource',
+        name: 'resource',
+        component: () => import(/* webpackChunkName: 'resource' */ '@/views/resource/index.vue')
+      },
+      {
+        path: '/course',
+        name: 'course',
+        component: () => import(/* webpackChunkName: 'course' */ '@/views/course/index.vue')
+      },
+      {
+        path: '/user',
+        name: 'user',
+        component: () => import(/* webpackChunkName: 'user' */ '@/views/user/index.vue')
+      },
+      {
+        path: '/advert',
+        name: 'advert',
+        component: () => import(/* webpackChunkName: 'advert' */ '@/views/advert/index.vue')
+      },
+      {
+        path: '/advert-space',
+        name: 'advert-space',
+        component: () => import(/* webpackChunkName: 'advert-space' */ '@/views/advert-space/index.vue')
+      }
+    ]
+  },
+  {
+    path: '*',
+    name: '404',
+    component: () => import(/* webpackChunkName: '404' */ '@/views/error-page/404.vue')
+  }
+]
+
+const router = new VueRouter({
+  routes
 })
 
 export default router
